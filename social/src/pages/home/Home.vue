74f526c557c5076952d83a3acb6d275c1b4b3069
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2022-01-15 18:22:15 -0300

    aula 9

---
+++ social/src/pages/home/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="">
+    <h2>Social</h2>
+    <div class="row">
+      <div class="col s12 m6">
+        <div class="card blue-grey darken-1">
+          <div class="card-content white-text">
+            <span class="card-title">Card Title</span>
+            <p>I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively.</p>
+          </div>
+          <div class="card-action">
+            <a href="#">This is a link</a>
+            <a href="#">This is a link</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
