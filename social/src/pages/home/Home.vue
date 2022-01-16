31b58837dd5a693d1a44eab6ee6edd5b5d3d6a3a
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2022-01-15 23:50:12 -0300

    Aula 17

--- social/src/pages/home/Home.vue
+++ social/src/pages/home/Home.vue
@@ -1,20 +1,36 @@
 <template>
-  <CardConteudoVue perfil="http://materializecss.com/images/yuna.jpg"
-  nome="Maria Silva"
-  data="13/01/18 13:30">
-  <CardDetalheVue img="http://materializecss.com/images/sample-1.jpg" titulo="Card Title" txt="I am a very simple card. I am good at containing small bits of information.
-    I am convenient because I require little markup to use effectively."/>
-
-  </CardConteudoVue>
+  <span>
+    <div class="row">
+      <GridVue class="input-field" tamanho="12">
+        <textarea id="textarea1" class="materialize-textarea"></textarea>
+        <label for="textarea1">O que esta acontecendo?</label>
+      </GridVue>
+      <p>
+        <GridVue tamanho="2 offset-s10" class="btn waves-effect waves-light">Publicar</GridVue>
+      </p>
+    </div>
+    <CardConteudoVue
+      perfil="http://materializecss.com/images/yuna.jpg"
+      nome="Maria Silva"
+      data="13/01/18 13:30"
+    >
+      <CardDetalheVue 
+        img="http://materializecss.com/images/sample-1.jpg"
+        titulo="Card Title" txt="I am a very simple card. I am good at containing small bits of information.
+        I am convenient because I require little markup to use effectively."
+      />
+    </CardConteudoVue>
+  </span>
 </template>
 
 <script>
 import CardConteudoVue from '@/components/social/CardConteudoVue'
 import CardDetalheVue from '@/components/social/CardDetalheVue'
+import GridVue from '@/components/layouts/GridVue'
 export default {
   name: 'Home',
   components:{
-    CardConteudoVue, CardDetalheVue
+    CardConteudoVue, CardDetalheVue, GridVue
   },
   data () {
     return {
