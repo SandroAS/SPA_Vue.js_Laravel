c122b5797fb603f977b4b19d880c95235a3d3064
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2022-01-16 11:46:33 -0300

    Aula 18

---
+++ social/src/components/social/PublicarConteudoVue.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="row">
+    <GridVue class="input-field" tamanho="12">
+      <textarea v-model="conteudo" class="materialize-textarea"></textarea>
+      <label>O que esta acontecendo?</label>
+    </GridVue>
+    <p>
+      <GridVue tamanho="2 offset-s10" class="btn waves-effect waves-light">Publicar</GridVue>
+    </p>
+  </div>
+</template>
+
+<script>
+import GridVue from '@/components/layouts/GridVue'
+export default {
+  name: "PublicarConteudoVue",
+  props: [],
+  components: {
+    GridVue
+  },
+  data () {
+    return {
+      conteudo: "",
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- social/src/pages/home/Home.vue
+++ social/src/pages/home/Home.vue
@@ -1,14 +1,6 @@
 <template>
   <span>
-    <div class="row">
-      <GridVue class="input-field" tamanho="12">
-        <textarea id="textarea1" class="materialize-textarea"></textarea>
-        <label for="textarea1">O que esta acontecendo?</label>
-      </GridVue>
-      <p>
-        <GridVue tamanho="2 offset-s10" class="btn waves-effect waves-light">Publicar</GridVue>
-      </p>
-    </div>
+    <PublicarConteudoVue/>
     <CardConteudoVue
       perfil="http://materializecss.com/images/yuna.jpg"
       nome="Maria Silva"
@@ -26,11 +18,11 @@
 <script>
 import CardConteudoVue from '@/components/social/CardConteudoVue'
 import CardDetalheVue from '@/components/social/CardDetalheVue'
-import GridVue from '@/components/layouts/GridVue'
+import PublicarConteudoVue from '@/components/social/PublicarConteudoVue'
 export default {
   name: 'Home',
   components:{
-    CardConteudoVue, CardDetalheVue, GridVue
+    CardConteudoVue, CardDetalheVue, PublicarConteudoVue
   },
   data () {
     return {
