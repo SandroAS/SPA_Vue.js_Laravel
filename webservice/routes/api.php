8e3e26a7a835107bdb2f0f30bdda9e62c69d1f4d
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2022-01-16 20:34:13 -0300

    Aula 27

--- webservice/routes/api.php
+++ webservice/routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/teste', function (Request $request) {
+    return $request->all();
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
