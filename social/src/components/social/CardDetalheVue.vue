261db5d28eb656e9d10f3883c14468c6c7901dc4
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2022-01-15 22:57:50 -0300

    Aula 16

---
+++ social/src/components/social/CardDetalheVue.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="card-image">
+      <img v-if="img" :src="img">
+      <span class="card-title">{{ titulo || ''}}</span>
+    </div>
+    <div v-if="txt" class="card-content">
+      <p>{{txt}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardDetalheVue",
+  props: ['img', 'titulo', 'txt'],
+}
+</script>
+
+<style>
+
+</style>
--- social/src/pages/home/Home.vue
+++ social/src/pages/home/Home.vue
@@ -2,23 +2,19 @@
   <CardConteudoVue perfil="http://materializecss.com/images/yuna.jpg"
   nome="Maria Silva"
   data="13/01/18 13:30">
-  <div class="card-image">
-    <img src="http://materializecss.com/images/sample-1.jpg">
-    <span class="card-title">Card Title</span>
-  </div>
-  <div class="card-content">
-    <p>I am a very simple card. I am good at containing small bits of information.
-    I am convenient because I require little markup to use effectively.</p>
-  </div>
+  <CardDetalheVue img="http://materializecss.com/images/sample-1.jpg" titulo="Card Title" txt="I am a very simple card. I am good at containing small bits of information.
+    I am convenient because I require little markup to use effectively."/>
+
   </CardConteudoVue>
 </template>
 
 <script>
 import CardConteudoVue from '@/components/social/CardConteudoVue'
+import CardDetalheVue from '@/components/social/CardDetalheVue'
 export default {
   name: 'Home',
   components:{
-    CardConteudoVue
+    CardConteudoVue, CardDetalheVue
   },
   data () {
     return {
